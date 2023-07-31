bb859a37c46f5ffe2abb89667e260d8535e32b1a
qianhuiwei <u1041216@utan.edu>
2023-07-31 21:06:59 +0800

    add button component

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Button from "./Button";
+import { FaPizzaSlice, FaBeer, FaGrinHearts, FaDizzy, FaTaxi } from "react-icons/fa";
+
+function App() {
+    const handleEvent = () => {
+        console.log("event detected");
+    }
+    return (
+        <div className="m-5">
+            <div>
+                <Button primary rounded onClick={handleEvent}>
+                    <FaPizzaSlice />
+                    Primary
+                </Button>
+            </div>
+            <div>
+                <Button secondary onMouse onMouseEnter={handleEvent}>
+                    <FaBeer />
+                    Secondary
+                </Button>
+            </div>
+            <div>
+                <Button success rounded outline onMouseLeave={handleEvent}>
+                    <FaGrinHearts />
+                    Success
+                </Button>
+            </div>
+            <div>
+                <Button warning>
+                    <FaDizzy />
+                    Warning
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    <FaTaxi />
+                    Danger
+                </Button>
+            </div>
+        </div>
+    )
+};
+
+export default App;
